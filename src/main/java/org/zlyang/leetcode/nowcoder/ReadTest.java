575d5a8d2e428c8a11b299f85b596be0b2f3dd7f
zlyang <565213263@qq.com>
2022-03-02 13:17:08 +0800

    add temp test

---
+++ src/main/java/org/zlyang/leetcode/nowcoder/ReadTest.java
@@ -0,0 +1,25 @@
+package org.zlyang.leetcode.nowcoder;
+
+import java.util.Scanner;
+
+/**
+ * @author ：zlyang
+ * @description：
+ * @date ：2022/3/1 11:56
+ */
+public class ReadTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        scanner.nextLine();
+        char[][] board = new char[n][m];
+        for(int i = 0; i < n; i++){
+            String s = scanner.nextLine();
+            System.out.println(s);
+            for(int j = 0; j < m; j++){
+
+            }
+        }
+    }
+}
